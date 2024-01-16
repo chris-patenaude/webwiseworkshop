015d1db3803523b7d212ccfc633947e8d620d1f6
Chris Patenaude <chris.patenaude@oregonstate.edu>
2024-01-16 15:05:30 -0800

    Add main activity route

---
+++ src/pages/learn.js
@@ -0,0 +1,9 @@
+export default function LearnView() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+                Hello World Time to learn
+            </div>
+        </main>
+    )
+}
